805c79224e3769de07230a7204a7221cb563c82f
Kwasi Gyasi-Agyei <kwasi@edged.agency>
2016-11-19 23:42:06 +0200

    Example uses missing functions
    Make use of datastore.NameKey(kind, name, parent) or datastore.IDKey(kind, int64, parent) not datastore.NewKey(context, kind, name, 0, nil)

--- datastore/datastore_quickstart/main.go
+++ datastore/datastore_quickstart/main.go
@@ -25,7 +25,6 @@ func main() {
 	// Your Google Cloud Platform project ID
 	projectID := "YOUR_PROJECT_ID"
 
-	// Creates a client
 	client, err := datastore.NewClient(ctx, projectID)
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
@@ -34,20 +33,21 @@ func main() {
 	// The kind for the new entity
 	kind := "Task"
 	// The name/ID for the new entity
-	name := "sampletask1"
+	//name := "sampletask1"
 	// The Cloud Datastore key for the new entity
-	taskKey := datastore.NewKey(ctx, kind, name, 0, nil)
+	key := datastore.IDKey(kind, 0, nil) 
+	//alternatively datastore.NameKey(kind, name, nil)
 
 	// Prepares the new entity
 	task := new(Task)
-	task.Description = "Buy milk"
+	task.Description = "Buy Milk"
 
 	// Saves the entity
-	if _, err := client.Put(ctx, taskKey, task); err != nil {
+	if _, err := client.Put(ctx, key, task); err != nil {
 		log.Fatalf("Failed to save task: %v", err)
 	}
 
-	fmt.Printf("Saved %v: %v", taskKey.String(), task.Description)
+	fmt.Printf("Saved %v: %v \n", key.String(), task.Description)
 }
 
 // [END datastore_quickstart]
